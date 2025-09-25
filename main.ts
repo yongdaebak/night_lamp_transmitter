beb4ac1634a6c2c486f3f928a4a1cf2b77269da7
Brigitte Tan <brigitte.tan@gmail.com>
2025-09-25 23:36:32 +0800

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,6 +1,7 @@
 let pir = 0
 radio.setGroup(1)
 basic.forever(function () {
+    basic.pause(100)
     pir = pins.digitalReadPin(DigitalPin.P0)
     radio.sendValue("sound", input.soundLevel())
     radio.sendValue("motion", pir)
